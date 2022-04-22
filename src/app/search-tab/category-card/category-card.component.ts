4fd7ff6569cdf5d1521db7359aa10e6365cff238
Jakub Wojciechowski <wojciechowski@tutanota.de>
2022-04-22 16:02:23 +0200

    Refactor category card component

--- src/app/search-tab/category-card/category-card.component.ts
+++ src/app/search-tab/category-card/category-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { Category } from 'src/app/data/category.model';
 
 @Component({
@@ -9,7 +9,14 @@ import { Category } from 'src/app/data/category.model';
 export class CategoryCardComponent implements OnInit {
   @Input() category: Category;
 
+  @HostBinding('style.background-image') background: string;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.background = `
+      linear-gradient(hsla(0, 0%, 15%, 0.5), hsla(0, 0%, 15%, 0.5)),
+      url('${this.category.photo}')
+    `;
+  }
 }
